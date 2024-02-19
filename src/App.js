289f40de9352bbd14625dc11d6a7399c8d1a4b83
RehamS21 <reham.mu433@gmail.com>
2024-02-19 09:06:18 +0300

    day-1 codes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-// let movies[]= [
-//   {
-//     title:"Exmple1",
-//     url:"",
-//     describistion:""
-//   },
-//   {
-//     title:"Exmple 2",
-//     url:"",
-//     describistion:""
-//   },
-//   {
-//     title:"Exmple 3",
-//     url:"",
-//     describistion:""
-//   }
-// ];
+const movies= [
+  {
+    title:"Exmple1",
+    url:'https://media.istockphoto.com/id/1566653954/photo/hands-holding-clapper-board-making-video-cinema-in-the-studio-movie-production-clapper-board.jpg?s=1024x1024&w=is&k=20&c=A6VodWvVPOMB2sZ6luvW0w1-dEjEkGdyBurLS54iNjQ=',
+    describistion:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title:"Exmple 2",
+    url:'https://media.istockphoto.com/id/1566653954/photo/hands-holding-clapper-board-making-video-cinema-in-the-studio-movie-production-clapper-board.jpg?s=1024x1024&w=is&k=20&c=A6VodWvVPOMB2sZ6luvW0w1-dEjEkGdyBurLS54iNjQ=',
+    describistion:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title:"Exmple 3",
+    url:'https://media.istockphoto.com/id/1566653954/photo/hands-holding-clapper-board-making-video-cinema-in-the-studio-movie-production-clapper-board.jpg?s=1024x1024&w=is&k=20&c=A6VodWvVPOMB2sZ6luvW0w1-dEjEkGdyBurLS54iNjQ=',
+    describistion:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  }
+];
 
 
 function App() {
 
   return ( // all things in return will appear to screen
     <div className="App">
-      {/* <Movie movies={movies[0]}></Movie> */}
+      <Movie movies={movies[0]}></Movie>
+      <Movie movies={movies[1]}></Movie>
+      <Movie movies={movies[2]}></Movie>
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,20 @@
 import React from 'react';
 
 const MovieImage = {
-    name: 'Movie photo',
-    imageUrl: 'https://media.istockphoto.com/id/1566653954/photo/hands-holding-clapper-board-making-video-cinema-in-the-studio-movie-production-clapper-board.jpg?s=1024x1024&w=is&k=20&c=A6VodWvVPOMB2sZ6luvW0w1-dEjEkGdyBurLS54iNjQ=',
-    imageSize: 90,
+    imageSize: 90
   };
   
 
 const Movie = ({movies}) => {
     return (
         <div>
-            <img src={MovieImage.imageUrl} 
+            <img src={movies.url} 
             style={{
-                width: MovieImage.imageSize,
-                height: MovieImage.imageSize
+                width: 90,
+                height: 90
             }}/>
-            <h1>Upgrade Your Experience</h1>
-            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-                took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <h1>{movies.title}</h1>
+            <p>{movies.describistion}</p>
         </div>
     );
 }
